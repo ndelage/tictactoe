c2894091d03a2b44891e02e5e20387cdf46c3f88
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-28 16:07:23 -0600

    respond to a double fork

--- lib/computer.rb
+++ lib/computer.rb
@@ -4,6 +4,7 @@ class Computer < Player
     return move_to_win if can_win?(self)
     return block if can_win?(game.player)
     return fork if can_fork?(self)
+    return force_win_block(self) if can_fork_twice?(game.player)
     return block_fork if can_fork?(game.player)
     return play_center if center_open?
     return play_opposite_corner if has_corner_and_opposite_open?(game.player)
@@ -13,6 +14,30 @@ class Computer < Player
 
   private
 
+  def force_win_block(player)
+    @block_move = nil
+    valid_moves.each do |index|
+      dup_game = deep_copy(@game)
+      dup_game.make_move(index.row, index.column, player.mark)
+      win = false
+      dup_game.board.open_indices.each do |indexx|
+        dup_game_again = deep_copy(dup_game)
+        dup_game_again.make_move(indexx.row, indexx.column, player.mark)
+        win = true if dup_game_again.game_over? == self
+      end
+      @block_move = index if win
+    end
+    @block_move
+  end
+
+  def can_fork_twice?(player)
+    fork_moves(player).size > 1
+  end
+
+  def can_fork?(player)
+    fork_moves(player).size > 0
+  end
+
   def play_opposite_corner
     @opposite
   end
@@ -20,9 +45,9 @@ class Computer < Player
   def has_corner_and_opposite_open?(player)
     @opposite = nil
     Board::CORNERS.each_with_index do |index, corner_index|
-      cell = game.board[index.row][index.column].marked_with?(player.mark)
+      cell = @game.board[index.row][index.column]
       indexxxx = Board::CORNERS[corner_index - 2]
-      opposite = game.board[indexxxx.row][indexxxx.column]
+      opposite = @game.board[indexxxx.row][indexxxx.column]
       @opposite = indexxxx if cell.marked_with?(player.mark) && opposite.empty?
     end
     @opposite
@@ -61,8 +86,8 @@ class Computer < Player
     @fork_move
   end
 
-  def can_fork?(player)
-    @fork_move = nil
+  def fork_moves(player)
+    @fork_moves = []
     valid_moves.each do |index|
       dup_game = deep_copy(@game)
       winning_combinations = 0
@@ -72,9 +97,9 @@ class Computer < Player
         dup_game_again.make_move(indexx.row, indexx.column, player.mark)
         winning_combinations += 1 if dup_game_again.game_over?
       end
-      @fork_move = index if winning_combinations >= 2
+      @fork_moves << index if winning_combinations >= 2
     end
-    @fork_move
+    @fork_moves
   end
 
 
