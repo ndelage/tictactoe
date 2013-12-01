53fe8b39e8f31346372ae44565541d59ec30bb22
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-01 17:00:01 -0600

    changed algorith to negamax

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,47 +1,34 @@
-require 'pry'
 class Computer < Player
-  
-  Negative_Infinity = -1.0/0.0
-  Positive_Infinity = 1.0/0.0
-
   def best_move(game)
-    best_score = Negative_Infinity
+    best_score = -Float::INFINITY
     best_index = nil
-    game.valid_moves.each do |index|
-      game.make_move(index.row, index.column)
-      score = minimax(game, false)
-      game.undo_move(index.row, index.column)
-      if score > best_score
-        best_score = score
-        best_index = index
-      end
+    try_each_valid_move(game) do |game, index|
+      score = -negamax(game, -1)
+      best_score, best_index = score, index if score > best_score
     end
     return best_index
   end
- 
-  def minimax(game, maximizing_player)
-    if game.game_over?
-      return 1 if game.winner == self
-      return -1 if game.winner == game.player
-      return 0
+
+  def negamax(game, color)
+    return color * get_score(game) if game.game_over?
+    best_score = -Float::INFINITY
+    try_each_valid_move(game) do |game|
+      best_score = [best_score, -negamax(game, -color)].max
     end
-    if maximizing_player
-      best_score = Negative_Infinity
-      game.valid_moves.each do |index|
-        game.make_move(index.row, index.column)
-        best_score = [best_score, minimax(game, false)].max
-        game.undo_move(index.row, index.column)
-      end
-      return best_score
-    else
-      best_score = Positive_Infinity
-      game.valid_moves.each do |index|
-        game.make_move(index.row, index.column)
-        best_score = [best_score, minimax(game, true)].min
-        game.undo_move(index.row, index.column)
-      end
-      return best_score
-    end
+    return best_score
   end
 
+  def get_score(game)
+    return 1 if game.winner == self
+    return -1 if game.winner == game.player
+    return 0
+  end
+
+  def try_each_valid_move(game)
+    game.valid_moves.each do |index|
+      game.make_move(index.row, index.column)
+      yield(game,index)
+      game.undo_move(index.row, index.column)
+    end
+  end
 end
